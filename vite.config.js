00e3a8ec0b63a071ee2d40ef3f7787782fcac456
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-07-25 13:07:37 +0330

    add themes styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePlugin from "vite-plugin-react-js-as-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePlugin([], {
+      jsxInject: true,
+      include: ["node_module/some-third-party-lib"],
+    }),
+  ],
 })
