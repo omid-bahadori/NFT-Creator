ff412fa926583686cfd9ccc6f6380b398527c0ef
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-07-25 13:14:18 +0330

    Customizing vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePlugin from "vite-plugin-react-js-as-jsx";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
