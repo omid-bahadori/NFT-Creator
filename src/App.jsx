3ee009ade863f84c6341706c9e0f853358d7c904
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-07-30 15:11:33 +0330

    add components to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+// Styles
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { light } from "./styles/Themes";
+
+// Components
+import Navigation from "./components/Navigation";
+import Home from "./components/sections/Home";
+import About from "./components/sections/About";
+import Roadmap from "./components/sections/Roadmap";
+import Showcase from "./components/sections/Showcase";
+import Team from "./components/sections/Team";
+import Faq from "./components/sections/Faq";
+import Footer from "./components/Footer";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <GlobalStyles />
+        <ThemeProvider theme={light}>
+          <Navigation />
+          <Home />
+          <About />
+          <Roadmap />
+          <Showcase />
+          <Team />
+          <Faq />
+          <Footer />
+        </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
