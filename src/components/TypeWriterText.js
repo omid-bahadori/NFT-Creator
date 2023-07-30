6f657fe3a10597cc393693688673cf1019e0fe4a
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-07-30 15:10:27 +0330

    add typer writer text to home section

---
+++ src/components/TypeWriterText.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+// components
+import Button from "./Button";
+
+// styles
+import { styled } from "styled-components";
+
+// type writer
+import Typewriter from "typewriter-effect";
+
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  width: 80%;
+  font-weight: 600;
+  color: ${(props) => props.theme.text};
+  align-self: flex-start;
+
+  span {
+    text-transform: uppercase;
+    font-family: "Akaya Telivigala", cursive;
+  }
+  .text-1 {
+    color: blue;
+  }
+  .text-2 {
+    color: orange;
+  }
+  .text-3 {
+    color: red;
+  }
+`;
+
+const SubTitle = styled.h3`
+  font-size: ${(props) => props.theme.fontlg};
+  text-transform: capitalize;
+  color: ${(props) => `rgba(${props.theme.textRgba}, 0.6)`};
+  font-weight: 600;
+  margin-bottom: 1rem;
+  width: 80%;
+  align-self: flex-start;
+`;
+
+const ButtonContainer = styled.div`
+    width: 80%;
+    align-self: flex-start;
+`;
+
+const TypeWriterText = () => {
+  return (
+    <>
+      <Title>
+        Discover A New Era Of Cool
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(`<span class="text-1">NFTs.</span>`)
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString(`<span class="text-2">Collectible items.</span>`)
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString(`<span class="text-3">Ape Killers!</span>`)
+              .pauseFor(2000)
+              .deleteAll()
+              .start();
+          }}
+        />
+      </Title>
+      <SubTitle>Bored Of Apes? Try Something New.</SubTitle>
+      <ButtonContainer>
+        <Button text="Explore" link="#about" />
+      </ButtonContainer>
+    </>
+  );
+};
+
+export default TypeWriterText;
