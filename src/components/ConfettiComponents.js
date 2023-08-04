d505d24d61bcd1ba25d85d8aff1019011d222b21
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-08-04 20:36:02 +0330

    create confetti component and use in team section

---
+++ src/components/ConfettiComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+
+const ConfettiComponents = () => {
+  const { width, height } = useWindowSize();
+
+  return <Confetti numberOfPieces={150} gravity={0.02} width={width} height={height / 1.7 } />;
+};
+
+export default ConfettiComponents;
