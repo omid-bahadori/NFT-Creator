1b8c8987bacdde162a2bacbf6457bf6ce230f299
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-08-05 21:23:57 +0330

    edit confetti height

--- src/components/ConfettiComponents.js
+++ src/components/ConfettiComponents.js
@@ -5,7 +5,7 @@ import Confetti from "react-confetti";
 const ConfettiComponents = () => {
   const { width, height } = useWindowSize();
 
-  return <Confetti numberOfPieces={150} gravity={0.02} width={width} height={height / 1.7 } />;
+  return <Confetti numberOfPieces={150} gravity={0.02} width={width} height={height / 1.5 } />;
 };
 
 export default ConfettiComponents;
