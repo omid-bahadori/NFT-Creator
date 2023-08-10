19232103b5e4b80bda895fecc788b35979a8713d
omid-bahadori <omid.bahadori.dev@gmail.com>
2023-08-10 12:51:42 +0330

    add scrollbar styles path

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../node_modules/normalize.css/normalize.css";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/scroll.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,4 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
- 
